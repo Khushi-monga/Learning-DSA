c56d1fb45747b5835ed847000c43e1b4b238b5e7
Khushi-monga <khushimonga20@gmail.com>
2026-03-11 13:50:26 +0530

    Implement stacks using LL

--- STRIVERS-A2Z-SHEET/stacks.cpp
+++ STRIVERS-A2Z-SHEET/stacks.cpp
@@ -2,78 +2,186 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//Implementing a stack using an array
+// //Implementing a stack using an array
 
- class Stack{
+//  class Stack{
+//     public:
+//     int *arr = NULL;
+//     int size;
+//     int top;
+
+//     Stack(int size){
+//         this -> size = size;
+//         this-> top = -1;
+//         this -> arr = new int[size];
+//     }
+
+//     void push(int data){
+//         //List is full (overflow)
+//         if(top == size - 1){
+//             cout << "Stack Overflow" << endl;
+//             return;
+//         }
+
+//         arr[++top] = data;
+//         return;
+//     }
+
+//     int pop(){
+//         //Empty list condition
+//         if(top == -1) {
+//             cout << "Stack Underflow" << endl;
+//             return INT_MIN;
+//         }
+
+//         int popped = arr[top--]; //store the element to be popped and move top 1 step back
+//         return popped;
+//     }
+
+//     int peek(){
+//         //empty list
+//         if(top == -1){
+//             cout << "Stack empty" << endl;
+//             return INT_MIN;
+//         }
+
+//         return arr[top];
+//     }
+
+//     int count(){
+//         return top + 1;
+//     }
+
+//     void print(){
+//         for(int i = 0; i<= top; i++){
+//             cout << arr[i] << " " ;
+//         } cout << endl;
+//     }
+//  };
+
+// Implementation of stack using LL
+
+//creating Node class first 
+class Node{
     public:
-    int *arr = NULL;
+    int data;
+    Node* next;
+
+    Node(int data){
+        this -> data = data;
+        this -> next = NULL;
+    }
+};
+
+//Converting an array to linked list 
+Node* convertArr2LL(vector<int> arr){
+    int length = arr.size();
+    Node* head = new Node(arr[0]);
+    Node* prev = head;
+
+    for(int i = 1; i<length; i++){
+        Node* temp = new Node(arr[i]);
+        prev -> next = temp;
+        prev = temp;
+    }
+
+    return head;
+}
+
+// creating the stack
+class Stack{
+    public:
     int size;
-    int top;
+    Node* top;
 
-    Stack(int size){
-        this -> size = size;
-        this-> top = -1;
-        this -> arr = new int[size];
+    Stack(){
+        this -> top = NULL;
+        this -> size = 0;
     }
 
-    void push(int data){
-        //List is full (overflow)
-        if(top == size - 1){
-            cout << "Stack Overflow" << endl;
-            return;
-        }
-
-        arr[++top] = data;
-        return;
+    void push(Node* &temp){
+        temp -> next = top;
+        top = temp;
+        size++;
     }
 
-    int pop(){
-        //Empty list condition
-        if(top == -1) {
-            cout << "Stack Underflow" << endl;
-            return INT_MIN;
-        }
-
-        int popped = arr[top--]; //store the element to be popped and move top 1 step back
-        return popped;
+    Node* pop(){
+        Node* temp = top;
+        top = top -> next;
+        temp -> next = NULL;
+        size--;
+        return temp;
     }
 
     int peek(){
-        //empty list
-        if(top == -1){
-            cout << "Stack empty" << endl;
+        if(top == NULL){
+            cout << "Stack Underflow";
             return INT_MIN;
         }
 
-        return arr[top];
+        return top -> data;
     }
 
     int count(){
-        return top + 1;
+        return size;
     }
 
     void print(){
-        for(int i = 0; i<= top; i++){
-            cout << arr[i] << " " ;
-        } cout << endl;
+        Node* temp = top;
+        while(temp != NULL){
+            cout << temp -> data << endl;
+            temp = temp -> next;
+        }
     }
- };
+    
+};
+
+void printLL(Node* &head){
+    Node* temp = head;
+
+    while(temp != nullptr){
+        temp = temp -> next;
+    }
+
+    cout << endl;
+}
 
 
  int main(){
 
     cout << endl;
-    Stack* st = new Stack(10);
-    st->push(20);
-    st->push(40);
-    st->push(60);
-    st->push(80);
+    // //Array implementation 
+    // Stack* st = new Stack(10);
+    // st->push(20);
+    // st->push(40);
+    // st->push(60);
+    // st->push(80);
+    // st->print();
+    // int pop = st->pop();
+    // cout << "Popped once" << endl;
+    // st->print();
+    // cout << "Top : " << st->peek() << endl;
+    // cout << "Total number of elements : " << st->count() << endl;
+
+    //LLimplementation
+    Stack* st = new Stack();
+    vector<int> sample = {10,20,30,40,50,60,70};
+    Node* head = convertArr2LL(sample);
+
+    printLL(head);
+
+    
+    // Pushing the elements of the linked list one by one to the stack
+    Node* temp = head;
+    while(temp != NULL){
+        Node* after = temp -> next;
+        st->push(temp);
+        temp = after;
+    }
+
+    //printing the stack
     st->print();
-    int pop = st->pop();
-    cout << "Popped once" << endl;
-    st->print();
-    cout << "Top : " << st->peek() << endl;
-    cout << "Total number of elements : " << st->count() << endl;
+    
 
     return 0;
  }
